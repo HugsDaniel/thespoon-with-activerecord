ddacf25c37d7c2b7d5488df3c3ba66cb70ed8a88
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-08-11 09:30:33 +0200

    Add addresss to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_071213) do
+ActiveRecord::Schema.define(version: 2020_08_11_071708) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "addresss"
   end
 
 end
